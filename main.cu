7ff9b46241208d467752887b6cf076afa6c2a2d6
Me-k-01 <mecaman01@gmail.com>
2022-11-26 17:36:40 +0100

    convertion rvb -> hsv

--- main.cu
+++ main.cu
@@ -2,6 +2,7 @@
 #include <string>
  
 #include "histoCPU.hpp" 
+#include "image.hpp" 
  
  void printUsage() 
 {
@@ -36,12 +37,17 @@ int main( int argc, char **argv )
 	// Version CPU
 	// Get input image
 	std::cout << "Loading image: " << fileName << std::endl;
-	//const Image inputImg 
-	//const PPMBitmap input( fileName );
-	//const int width		= input.getWidth();
-	//const int height	= input.getHeight();
-	//std::cout << "Image has " << width << " x " << height << " pixels" << std::endl;
-
+	Image inputImg;
+	inputImg.load(fileName);
+	std::cout << "Image has " << inputImg._width << " x " << inputImg._height << " pixels" << std::endl;
+	std::cout << "and has " << inputImg._nbChannels << " channels of color" << std::endl;
+	size_t size = inputImg._width * inputImg._height * 3 ;
+	unsigned char hsv[3][size] = {{},{},{}};   
+	rgb2hsv(&inputImg, hsv[0], hsv[1], hsv[2]);
+	//unsigned char hue[size]; 
+	//unsigned char sat[size];
+	//unsigned char val[size]; 
+	//rgb2hsv(&inputImg, hue, sat, val);
 
 	return 0;
 }
