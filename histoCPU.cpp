25338c6763f412cdb9b220a044ab90636943ec2b
Me-k-01 <mecaman01@gmail.com>
2022-12-02 17:26:53 +0100

    Ajustement mineur

--- histoCPU.cpp
+++ histoCPU.cpp
@@ -110,10 +110,8 @@ int repart(unsigned int * histArray, size_t l) {
 }
 
 // Application de la transformation sur la composante intensité de l'image
-void equalization(unsigned char * imgVal, size_t imgSize, unsigned int * histArray) {
-    float L = 256.f; 
-
+void equalization(unsigned char * imgVal, size_t imgSize, unsigned int * histArray) { 
     for (size_t x = 0; x < imgSize; x++) {
-        imgVal[x] = (L - 1.f) * 255.f / (L * imgSize) * (float)repart(histArray, imgVal[x]);
+        imgVal[x] = 255.f / imgSize * repart(histArray, imgVal[x]); // On a multiplier par 255 pour avoir une valeur entre 0 et 255
     }
 }
